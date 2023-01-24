b954308c5ab15c8f1b0f7bf294c3e11f74410506
Steve468 <112678562+Steve468@users.noreply.github.com>
2023-01-24 12:37:35 +0530

    Update 3tier application deployment.ps1

--- 3tier application deployment.ps1
+++ 3tier application deployment.ps1
@@ -11,24 +11,25 @@ $subnet1 = Add-AzVirtualNetworkSubnetConfig -Name "appsubnet1" -AddressPrefix "1
 $subnet2 = Add-AzVirtualNetworkSubnetConfig -Name "websubnet1" -AddressPrefix "10.0.2.0/24" -VirtualNetwork $vnet
 $subnet3 = Add-AzVirtualNetworkSubnetConfig -Name "dbsubnet1" -AddressPrefix "10.0.3.0/24" -VirtualNetwork $vnet
 
+# Create availability sets
+$avSet1 = New-AzAvailabilitySet -Name "webServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2 -Sku Aligned
+$avSet2 = New-AzAvailabilitySet -Name "appServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2 -Sku Aligned
+$avSet3 = New-AzAvailabilitySet -Name "dbServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2 -Sku Aligned
+
 
 # Create virtual machines for each tier
-$vm1 = New-AzVM -Name "webServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "appsubnet1" -AddressPrefix "10.0.1.0/24" -PublicIpAddressName "webServerIp" -OpenPorts 80
-$vm2 = New-AzVM -Name "appServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "websubnet1" -AddressPrefix "10.0.2.0/24" -PublicIpAddressName "appServerIp" -OpenPorts 8080
-$vm3 = New-AzVM -Name "dbServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "dbsubnet1" -AddressPrefix "10.0.3.0/24" -PublicIpAddressName "dbServerIp" -OpenPorts 1433
+$vm1 = new-AzVM -Name "webServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "appsubnet1" -AddressPrefix "10.0.1.0/24" -PublicIpAddressName "webServerIp" -OpenPorts 80 -AvailabilitySetName $avSet1.Name
+$vm2 = New-AzVM -Name "appServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "websubnet1" -AddressPrefix "10.0.2.0/24" -PublicIpAddressName "appServerIp" -OpenPorts 8080 -AvailabilitySetName $avSet2.Name
+$vm3 = New-AzVM -Name "dbServer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -VirtualNetworkName $vnet.Name -SubnetName "dbsubnet1" -AddressPrefix "10.0.3.0/24" -PublicIpAddressName "dbServerIp" -OpenPorts 1433 -AvailabilitySetName $avSet3.Name
 
-# Create availability sets
-$avSet1 = New-AzAvailabilitySet -Name "webServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2
-$avSet2 = New-AzAvailabilitySet -Name "appServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2
-$avSet3 = New-AzAvailabilitySet -Name "dbServerAvSet" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 2
 
-# Add virtual machines to availability sets
-Add-AzVmToAvailabilitySet -VM $vm1 -AvailabilitySet $avSet1
-Add-AzVmToAvailabilitySet -VM $vm2 -AvailabilitySet $avSet2
-Add-AzVmToAvailabilitySet -VM $vm3 -AvailabilitySet $avSet3
+$publicip = New-AzPublicIpAddress -ResourceGroupName $resourceGroup.ResourceGroupName -Name "lb-ip" -Location $resourceGroup.Location -AllocationMethod "Dynamic"
+$frontend = New-AzLoadBalancerFrontendIpConfig -Name "MyFrontEnd" -PublicIpAddress $publicip
 
-# Create load balancer
-$lb = New-AzLoadBalancer -Name "myLoadBalancer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -FrontendIpConfigurationName "lbFrontendIp" -BackendPoolName "lbBackendPool"
+$backendAddressPool = New-AzLoadBalancerBackendAddressPoolConfig -Name "MyBackendAddPoolConfig02"
+$probe = New-AzLoadBalancerProbeConfig -Name "MyProbe" -Protocol "http" -Port 80 -IntervalInSeconds 15 -ProbeCount 2 -ProbeThreshold 2 -RequestPath "healthcheck.aspx"
+$inboundNatRule1 = New-AzLoadBalancerInboundNatRuleConfig -Name "MyinboundNatRule1" -FrontendIPConfiguration $frontend -Protocol "Tcp" -FrontendPort 3389 -BackendPort 3389 -IdleTimeoutInMinutes 15 -EnableFloatingIP
+$inboundNatRule2 = New-AzLoadBalancerInboundNatRuleConfig -Name "MyinboundNatRule2" -FrontendIPConfiguration $frontend -Protocol "Tcp" -FrontendPort 3391 -BackendPort 3392
+$lbrule = New-AzLoadBalancerRuleConfig -Name "MyLBruleName" -FrontendIPConfiguration $frontend -BackendAddressPool $backendAddressPool -Probe $probe -Protocol "Tcp" -FrontendPort 80 -BackendPort 80 -IdleTimeoutInMinutes 15 -EnableFloatingIP -LoadDistribution SourceIP
+$lb = New-AzLoadBalancer -Name "MyLoadBalancer" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $resourceGroup.Location -FrontendIpConfiguration $frontend -BackendAddressPool $backendAddressPool -Probe $probe -InboundNatRule $inboundNatRule1,$inboundNatRule2 -LoadBalancingRule $lbrule
 
-# Create load balancer rules
-$lbRule1 = New-AzLoadBalancerRuleConfig -Name "httpRule" -FrontendIpConfiguration $lb.FrontendIpConfigurations[0] -BackendAddressPool $lb.BackendAddressPools[
